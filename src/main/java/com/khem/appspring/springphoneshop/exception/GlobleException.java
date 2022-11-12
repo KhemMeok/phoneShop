2418fdf8936cce2bef12d1b2e2ada98c71ed4dbe
Meok Khem <113187567+KhemMeok@users.noreply.github.com>
2022-11-12 10:39:35 +0700

    delete todo

--- src/main/java/com/khem/appspring/springphoneshop/exception/GlobleException.java
+++ src/main/java/com/khem/appspring/springphoneshop/exception/GlobleException.java
@@ -13,8 +13,6 @@ public class GlobleException {
     public ResponseEntity<?> handleHttpClientError(ApiException e) {
         //ServiceException exception = new ServiceException(e.getStatusCode(), e.getMessage());
 
-        //TODO customize respond DTO message
-
         ErrorResponse errorResponse=new ErrorResponse(e.getStatus().getReasonPhrase(), e.getMessage());
 
         return ResponseEntity.status(e.getStatus()).body(errorResponse);
