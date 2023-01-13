6764bcbd972cce50990c49bc008e63080c0f1fce
Meok Khem <113187567+KhemMeok@users.noreply.github.com>
2023-01-13 21:26:45 +0700

    set_sale

---
+++ src/main/java/com/khem/appspring/springphoneshop/dto/PriceDTO.java
@@ -0,0 +1,8 @@
+package com.khem.appspring.springphoneshop.dto;
+
+import lombok.Data;
+
+@Data
+public class PriceDTO {
+	private double salePrice;
+}
--- src/main/java/com/khem/appspring/springphoneshop/service/ProductService.java
+++ src/main/java/com/khem/appspring/springphoneshop/service/ProductService.java
@@ -6,4 +6,5 @@ import com.khem.appspring.springphoneshop.model.Product;
 public interface ProductService {
     Product save(ProductImportDTO productImportDTO);
     Product getById(Long id);
+    Product setPrice(Long productId,Double price);
 }
