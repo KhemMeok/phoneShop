556352a5a919ad8e8ab039c0caf7a4a2e937d31c
Meok Khem <113187567+KhemMeok@users.noreply.github.com>
2023-01-28 09:06:59 +0700

    swagger-serror"

--- src/main/java/com/khem/appspring/springphoneshop/mapper/ModelEntityMapper.java
+++ src/main/java/com/khem/appspring/springphoneshop/mapper/ModelEntityMapper.java
@@ -9,9 +9,9 @@ import com.khem.appspring.springphoneshop.model.Model;
 import com.khem.appspring.springphoneshop.service.BrandService;
 
 @Mapper(componentModel = "spring" ,uses={BrandService.class})
-public interface ModelMapper {
+public interface ModelEntityMapper {
 
-    ModelMapper INSTANCE = Mappers.getMapper(ModelMapper.class);
+    ModelEntityMapper INSTANCE = Mappers.getMapper(ModelEntityMapper.class);
 
      @Mapping(target = "brand" ,source = "dto.brandId")
     Model toModel(ModelDTO dto);
