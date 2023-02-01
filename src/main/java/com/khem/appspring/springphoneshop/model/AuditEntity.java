d9e9365869c917e5278d989090fea1df337ccbf8
Meok Khem <113187567+KhemMeok@users.noreply.github.com>
2023-02-01 22:20:34 +0700

    fin

--- src/main/java/com/khem/appspring/springphoneshop/model/AuditEntity.java
+++ src/main/java/com/khem/appspring/springphoneshop/model/AuditEntity.java
@@ -3,14 +3,17 @@ package com.khem.appspring.springphoneshop.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Data;
 @MappedSuperclass
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AuditEntity {
 
     @CreatedDate
